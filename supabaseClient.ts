8c31bafb0d5c94f2b43ff5255eddb32d818d6612
Horashio1 <135460969+Horashio1@users.noreply.github.com>
2024-10-15 00:24:21 +0530

    include .env file

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,7 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your actual Supabase project credentials
-const supabaseUrl = 'https://kbcaevsuxnajykrzhjco.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiY2FldnN1eG5hanlrcnpoamNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyMDE2MDYsImV4cCI6MjA0Mjc3NzYwNn0.X3Zi7DmT_Y8Xbltx-tPzhrfhz3AhIs3lb59RrpLMBfY';
+// Use environment variables for Supabase credentials
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
