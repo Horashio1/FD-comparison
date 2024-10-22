baa94395cbface32a95c42e41853659a5d142073
Horashio1 <135460969+Horashio1@users.noreply.github.com>
2024-10-22 23:41:36 +0530

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,40 +2,17 @@ import type { Metadata } from "next";
 // import localFont from "next/font/local";
 import { Inter } from 'next/font/google'
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export const metadata: Metadata = {
   title: 'Fixed Deposit Comparison',
   description: 'Compare fixed deposit rates across Sri Lankan banks',
 }
 
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
 
 export default function RootLayout({
   children,
@@ -44,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<Analytics /></body>
     </html>
   )
 }
