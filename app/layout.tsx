2afcb9fbae334418dcdcbc76485789bdb87ef59d
Horashio1 <135460969+Horashio1@users.noreply.github.com>
2024-12-25 13:19:03 +0530

    Meta Metadata in layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,30 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer'; // Import Footer component
 
 export const metadata: Metadata = {
-  title: 'Best Rates in Sri Lanka',
-  description: 'Compare banking rates in Sri Lanka',
+  title: "Best Rates in Sri Lanka",
+  description: "Compare banking rates in Sri Lanka",
+  openGraph: {
+    title: "BestRates.lk",
+    description: "Compare banking rates and Card Deals in Sri Lanka",
+    url: "https://www.bestrates.lk",
+    siteName: "BestRates",
+    images: [
+      {
+        url: "https://kbcaevsuxnajykrzhjco.supabase.co/storage/v1/object/public/marketing/cards/haresha_credit_card_promotions_--v_6.1_dec98e47-898b-4e82-919c-013f9b7aad1e_3.png",
+        width: 1200,
+        height: 630,
+        alt: "Logo or description of the image",
+      },
+    ],
+    type: "website",
+  },
+  // Twitter tags are optional, but they don't hurt:
+  twitter: {
+    card: "summary_large_image",
+    title: "BestRates.lk",
+    description: "Compare banking rates and Card Deals in Sri Lanka",
+    images: ["https://kbcaevsuxnajykrzhjco.supabase.co/storage/v1/object/public/marketing/cards/haresha_credit_card_promotions_--v_6.1_dec98e47-898b-4e82-919c-013f9b7aad1e_3.png"],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
